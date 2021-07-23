830a41c64d6d3243500cc11d8b061410483ef540
mika1119 <mika1119.work@gmail.com>
2021-07-23 12:23:47 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'front_pages/contact'
-  get 'front_pages/information'
+  get 'contact', to: 'front_pages#contact', as: 'contact'
+  get 'information', to: 'front_pages#information' as: 'information'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
